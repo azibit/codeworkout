a36545769fd42efeb4be09f3e102f172c6665368
azibit <abdulazeezidris28@gmail.com>
2016-04-23 14:31:27 +0100

    Refactor .gitignore file

--- src/main/java/com/codeout/inspaya/Inspaya.java
+++ src/main/java/com/codeout/inspaya/Inspaya.java
@@ -5,10 +5,31 @@
  */
 package com.codeout.inspaya;
 
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
+
 /**
  *
  * @author azibit
  */
 public class Inspaya {
-    
+
+    public static void main(String[] args) throws PayPalRESTException {
+        
+        String CLIENT_ID = "AR7IAGiGhNlsJo97VbmADCSjMZUZB3wcorcjJaibM0ia88H"
+                + "bPg4_owcN8Qi0dqOk0G2MKZO4BuOod7Y-";
+        
+        String CLIENT_SECRET = "EAkSAo-Ak_zU-oxTx6mSBFwDehlcOtwSStiHhfnAICqjC"
+                + "W_4An0Wip98Lwi9793dbpiSaog_Xo_kp331";
+        
+        
+        OAuthTokenCredential tokenCredential
+                = new OAuthTokenCredential("<" + CLIENT_ID + ">", 
+                        "<" + CLIENT_SECRET + ">");
+
+        String accessToken = tokenCredential.getAccessToken();
+        
+        System.out.println("Access Token: " + accessToken);
+    }
+
 }
